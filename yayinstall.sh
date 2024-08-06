03d8f7cae37298ffbc2241c78b2610c4b115829b
Siiiinj <jeremiervo@gmail.com>
2024-08-06 03:34:45 +0200

    [FIX] repo link

--- yayinstall.sh
+++ yayinstall.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --needed git base-devel fakeroot
-git clone https://github.com/aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/yay-git.git 
+cd yay-git
 makepkg -si
 echo "yay should be correctly installed !"
